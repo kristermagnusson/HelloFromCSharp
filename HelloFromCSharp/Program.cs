a86ca27c93a2ef48744cb47a24721c8dedf4e16e
kma <kma@RUTAN>
2022-05-10 14:43:41 +0200

    Add project files.

---
+++ HelloFromCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Please enter your firstname:");
+string firstname = Console.ReadLine();
+Console.WriteLine("Please enter your second name");  //generic comment
+string lastname = Console.ReadLine();
+Console.WriteLine("Hello " + firstname + " " + lastname);
